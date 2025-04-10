2983a20975000f4105440e36f7b93476c527cd04
Kritigupta20604 <125851604+Kritigupta20604@users.noreply.github.com>
2025-04-10 18:12:26 +0000

    conversions

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,22 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "kriti"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;   0 => false
+// "" => false
+// "kriti" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
