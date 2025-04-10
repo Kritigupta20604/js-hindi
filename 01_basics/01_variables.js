dfa7f997fc39cca9da25dc9b1399173e27e56472
Kritigupta20604 <125851604+Kritigupta20604@users.noreply.github.com>
2025-04-10 15:59:44 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123687
+let accountEmail = "kriti@google.com"
+var accountPassword = "3465"
+accountCity = "jaipur"
+let accountState;
+
+/*
+prefer not to use var/*
+
+// accountId = 2
+
+accountEmail = "kay2@gmail.com"
+accountPassword = "231897392"
+accountCity = "goa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello")
+
+let name = "kriti"
+let age = 18
+let isLoggedIn= false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => to find uniqueness
+
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
