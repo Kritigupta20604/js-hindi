7f36d18e9b142a427a80c2d05290c5eba9485a06
Kriti Gupta <125851604+Kritigupta20604@users.noreply.github.com>
2025-06-30 16:20:10 +0000

    math and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.5648
+
+// console.log(otherNumber.toPrecision(3));
+
+// ++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.2));
+// console.log(Math.min(4, 3, 5, 7));
+// console.log(Math.max(4, 5, 6, 8));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+const min = 10
+const max = 20
+
+console.log((Math.floor(Math.random() * (max - min + 1))) + min)
