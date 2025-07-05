7c35247a812d88cc4c12419d718f213af4d12098
Kriti Gupta <125851604+Kritigupta20604@users.noreply.github.com>
2025-07-05 12:33:49 +0000

    object1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,37 @@
+// singleton
+// object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name : "kriti",
+    "full name": "kriti gupta",
+    [mySym]: "mykey1",
+    age: 21,
+    location: "Jaipur",
+    email: "kriti@google.com",
+    isLoggesIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "kritigupta@google.com"
+// Object.freeze(JsUser)
+JsUser.email = "kriti@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
