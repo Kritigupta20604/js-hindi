63588b21afcc13daae25a66b51f683f3bdb9ee4d
Kriti Gupta <125851604+Kritigupta20604@users.noreply.github.com>
2025-07-09 16:39:55 +0000

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+if (temperature === 50){
+    // console.log("less than 50");
+    
+} else{
+    // console.log("temeprature is grater than 50 ");
+}
+// console.log("execute");
+// <, >, <=, >=, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`); 
+// }
+
+// console.log(`user power: ${power}`); 
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test");
+// console.log("test2");
+
+// if (balance < 500){
+//     console.log("less then 500");    
+// }
+// else if(balance < 750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less then 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedinFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedinFromEmail){
+    console.log("user logged in");
+}
